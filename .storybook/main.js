6af190c5a193596ac540eedaff6451b37b728141
Mikołaj Szymczak <szymczak.mikolaj08@gmail.com>
2020-07-19 19:26:25 +0200

    storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@
     stories: ["../src/**/*.story.tsx"],
     addons: [
         "@storybook/preset-create-react-app",
-        "@storybook/addon-actions",
         "@storybook/addon-links",
         {
             name: "@storybook/addon-docs",
