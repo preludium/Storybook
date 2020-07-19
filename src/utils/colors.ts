d9d112e112596ec35b8dc3b1b2419c7b0e2a1666
Mikołaj Szymczak <szymczak.mikolaj08@gmail.com>
2020-07-19 19:25:34 +0200

    color file

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,125 +1,194 @@
-import color from "color";
+const Lavender = "#E6E6FA";
+const Thistle = "#D8BFD8";
+const Plum = "#DDA0DD";
+const Orchid = "#DA70D6";
+const Violet = "#EE82EE";
+const Fuchsia = "#FF00FF";
+const Magenta = "#FF00FF";
+const MediumOrchid = "#BA55D3";
+const DarkOrchid = "#9932CC";
+const DarkViolet = "#9400D3";
+const BlueViolet = "#8A2BE2";
+const DarkMagenta = "#8B008B";
+const Purple = "#800080";
+const MediumPurple = "#9370DB";
+const MediumSlateBlue = "#7B68EE";
+const SlateBlue = "#6A5ACD";
+const DarkSlateBlue = "#483D8B";
+const RebeccaPurple = "#663399";
+const Indigo = "#4B0082";
 
-const rgba = (hex: string, opacity: number) => {
-    return color(hex).alpha(opacity).rgb().string();
+const purple = {
+    Lavender, Thistle, Plum, Orchid, Violet, Fuchsia, Magenta,
+    MediumOrchid, DarkOrchid, DarkViolet, BlueViolet, DarkMagenta,
+    Purple, MediumPurple, MediumSlateBlue, SlateBlue, DarkSlateBlue,
+    RebeccaPurple, Indigo
+}
+
+const CadetBlue = "#5F9EA0";
+const SteelBlue = "#4682B4";
+const LightSteelBlue = "#B0C4DE";
+const LightBlue = "#ADD8E6";
+const PowderBlue = "#B0E0E6";
+const LightSkyBlue = "#87CEFA";
+const SkyBlue = "#87CEEB";
+const CornflowerBlue = "#6495ED";
+const DeepSkyBlue = "#00BFFF";
+const DodgerBlue = "#1E90FF";
+const RoyalBlue = "#4169E1";
+const Blue = "#0000FF";
+const MediumBlue = "#0000CD";
+const DarkBlue = "#00008B";
+const Navy = "#000080";
+const MidnightBlue = "#191970";
+
+const blue = {
+    CadetBlue, SteelBlue, LightBlue, LightSteelBlue,
+    PowderBlue, LightSkyBlue, SkyBlue, CornflowerBlue, DeepSkyBlue,
+    DodgerBlue, RoyalBlue, Blue, MediumBlue, DarkBlue, Navy, MidnightBlue
 };
 
-// Background colors
-const bgPrimary = "#fff";
-const bgSecondary = "#f4fbfe";
-const bgOverlay = "#000";
-const bgDark = rgba("#000", 0.96);
-const bgMap = "#f1f4f6";
+const LightSalmon = "#FFA07A";
+const Salmon = "#FA8072";
+const DarkSalmon = "#E9967A";
+const LightCoral = "#F08080";
+const IndianRed = "#CD5C5C";
+const Crimson = "#DC143C";
+const Red = "#FF0000";
+const FireBrick = "#B22222";
+const DarkRed = "#8B0000";
 
-// Link colors
-const linkPrimary = "#297cbb";
-const linkPrimaryHover = rgba(linkPrimary, 0.7);
-
-// Border colors
-const borderPrimary = "#e4e4e4";
-const borderLight = "#bfd0d8";
-
-// Text colors
-const textPrimary = "#2c3643";
-const textSecondary = rgba(textPrimary, 0.7);
-const textOverlay = "#fff";
-const textLight = "#576576";
-
-// Shadow colors
-const shadowPrimary = "#000";
-
-// Accent colors
-const accentBlue = "#88bde7";
-const accentGray = "#b6c3ca";
-const accentGreen = "#16c98d";
-const accentOrange = "#ff882e";
-const accentPink = "#ff6e8d";
-const accentPurple = "#9d69c9";
-const accentRed = "#da0909";
-const accentYellow = "#ffc83f";
-
-// UI colors
-const uiGreen = "#44db5e";
-
-// Social colors
-const socialFacebook = "#3a5999";
-const socialFacebookMessenger = "#1472fb";
-const socialPinterest = "#cb2027";
-const socialGoogleBlue = "#557ebf";
-const socialGoogleGreen = "#36a852";
-const socialGoogleYellow = "#f9bc15";
-const socialGoogleRed = "#ea4535";
-const socialReddit = "#fc4220";
-const socialTwitter = "#1da1f2";
-const socialWhatsapp = "#28eb76";
-const socialWeChat = "#7bb32e";
-
-// POI colors
-// These are deprecated and will be removed in the next major release
-const poiGreen = accentGreen;
-const poiBlue = accentBlue;
-const poiYellow = accentYellow;
-const poiRed = accentRed;
-const poiPink = accentPink;
-const poiPurple = accentPurple;
-const poiGray = accentGray;
-const poiDefault = "#a8a9ae";
-const poiEat = "#00ca8b";
-const poiPlay = "#ffc92a";
-const poiSee = "#f8324d";
-
-export default {
-    bgPrimary,
-    bgSecondary,
-    bgOverlay,
-    bgDark,
-    bgMap,
-
-    linkPrimary,
-    linkPrimaryHover,
-
-    borderPrimary,
-    borderLight,
-
-    textPrimary,
-    textSecondary,
-    textOverlay,
-    textLight,
-
-    shadowPrimary,
-
-    accentBlue,
-    accentGray,
-    accentGreen,
-    accentOrange,
-    accentPink,
-    accentPurple,
-    accentRed,
-    accentYellow,
-
-    uiGreen,
-
-    poiGreen,
-    poiBlue,
-    poiYellow,
-    poiRed,
-    poiPink,
-    poiPurple,
-    poiGray,
-    poiDefault,
-    poiEat,
-    poiPlay,
-    poiSee,
-
-    socialFacebook,
-    socialFacebookMessenger,
-    socialGoogleBlue,
-    socialGoogleGreen,
-    socialGoogleYellow,
-    socialGoogleRed,
-    socialPinterest,
-    socialReddit,
-    socialTwitter,
-    socialWhatsapp,
-    socialWeChat
+const red = {
+    LightSalmon, Salmon, DarkSalmon, LightCoral, IndianRed, Crimson,
+    Red, FireBrick, DarkRed
 };
+
+const Gold = "#FFD700";
+const Yellow = "#FFFF00";
+const LightYellow = "#FFFFE0";
+const LemonChiffon = "#FFFACD";
+const LightGoldenRodYellow = "#FAFAD2";
+const PapayaWhip = "#FFEFD5";
+const Moccasin = "#FFE4B5";
+const PeachPuff = "#FFDAB9";
+const PaleGoldenRod = "#EEE8AA";
+const Khaki = "#F0E68C";
+const DarkKhaki = "#BDB76B";
+
+const yellow = {
+    Gold, Yellow, LightYellow, LemonChiffon, LightGoldenRodYellow,
+    PapayaWhip, Moccasin, PeachPuff, PaleGoldenRod, Khaki, DarkKhaki
+}
+
+const LawnGreen = "#7CFC00";
+const Lime = "#00FF00";
+const LimeGreen = "#32CD32";
+const PaleGreen = "#98FB98";
+const LightGreen = "#90EE90";
+const MediumSpringGreen = "#00FA9A";
+const SpringGreen = "#00FF7F";
+const MediumSeaGreen = "#3CB371";
+const SeaGreen = "#2E8B57";
+const ForestGreen = "#228B22";
+const Green = "#008000";
+const DarkGreen = "#006400";
+const YellowGreen = "#9ACD32";
+const OliveDrab = "#6B8E23";
+const DarkOliveGreen = "#556B2F";
+const MediumAquaMarine = "#66CDAA";
+const DarkSeaGreen = "#8FBC8F";
+const LightSeaGreen = "#20B2AA";
+const DarkCyan = "#008B8B";
+const Teal = "#008080";
+
+const green = {
+    LawnGreen, Lime, LimeGreen, PaleGreen, LightGreen, MediumSpringGreen,
+    SpringGreen, MediumSeaGreen, SeaGreen, ForestGreen, Green,
+    DarkGreen, YellowGreen, OliveDrab, DarkOliveGreen, MediumAquaMarine,
+    DarkSeaGreen, LightSeaGreen, DarkCyan, Teal
+};
+
+const Aqua = "#00FFFF";
+const Cyan = "#00FFFF";
+const LightCyan = "#E0FFFF";
+const PaleTurquoise = "#AFEEEE";
+const Aquamarine = "#7FFFD4";
+const Turquoise = "#40E0D0";
+const MediumTurquoise = "#48D1CC";
+const DarkTurquoise = "#00CED1";
+
+const cyan = {
+    Aqua, Cyan, LightCyan, PaleTurquoise, Aquamarine, Turquoise,
+    MediumTurquoise, DarkTurquoise
+};
+
+const Cornsilk = "#FFF8DC";
+const BlanchedAlmond = "#FFEBCD";
+const Bisque = "#FFE4C4";
+const NavajoWhite = "#FFDEAD";
+const Wheat = "#F5DEB3";
+const BurlyWood = "#DEB887";
+const Tan = "#D2B48C";
+const RosyBrown = "#BC8F8F";
+const SandyBrown = "#F4A460";
+const GoldenRod = "#DAA520";
+const DarkGoldenRod = "#B8860B";
+const Peru = "#CD853F";
+const Chocolate = "#D2691E";
+const Olive = "#808000";
+const SaddleBrown = "#8B4513";
+const Sienna = "#A0522D";
+const Brown = "#A52A2A";
+const Maroon = "#800000";
+
+const brown = {
+    Cornsilk, BlanchedAlmond, Bisque, NavajoWhite, Wheat, BurlyWood, Tan,
+    RosyBrown, SandyBrown, GoldenRod, DarkGoldenRod, Peru, Chocolate,
+    Olive, SaddleBrown, Sienna, Brown, Maroon
+};
+
+const White = "#FFFFFF";
+const Snow = "#FFFAFA";
+const HoneyDew = "#F0FFF0";
+const MintCream = "#F5FFFA";
+const Azure = "#F0FFFF";
+const AliceBlue = "#F0F8FF";
+const GhostWhite = "#F8F8FF";
+const WhiteSmoke = "#F5F5F5";
+const SeaShell = "#FFF5EE";
+const Beige = "#F5F5DC";
+const OldLace = "#FDF5E6";
+const FloralWhite = "#FFFAF0";
+const Ivory = "#FFFFF0";
+const AntiqueWhite = "#FAEBD7";
+const Linen = "#FAF0E6";
+const LavenderBlush = "#FFF0F5";
+const MistyRose = "#FFE4E1";
+
+const white = {
+    White, Snow, HoneyDew, MintCream, Azure, AliceBlue, GhostWhite,
+    WhiteSmoke, SeaShell, Beige, OldLace, FloralWhite, Ivory,
+    AntiqueWhite, Linen, LavenderBlush, MistyRose
+};
+
+const Gainsboro = "#DCDCDC";
+const LightGray = "#D3D3D3";
+const Silver = "#C0C0C0";
+const DarkGray = "#A9A9A9";
+const DimGray = "#696969";
+const Gray = "#808080";
+const LightSlateGray = "#778899";
+const SlateGray = "#708090";
+const DarkSlateGray = "#2F4F4F";
+const Black = "#000000";
+
+const gray = {
+    Gainsboro, LightGray, Silver, DarkGray, DimGray, Gray,
+    LightSlateGray, SlateGray, DarkSlateGray, Black
+};
+
+const color = {
+    purple, blue, red, yellow, green, cyan, brown, white, gray
+};
+
+export default color;
